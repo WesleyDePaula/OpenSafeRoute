28f8c3928d5b353c37358331e5a1c1f8df01ad9d
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-08-17 19:58:31 -0300

    alterando a chamada de API na classe Trajeto
    alterando a chamada de API na classe Trajeto

--- src/main/java/modelo/entidade/mapa/Trajeto.java
+++ src/main/java/modelo/entidade/mapa/Trajeto.java
@@ -1,5 +1,10 @@
 package modelo.entidade.mapa;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+
 import org.geojson.LineString;
 
 import modelo.enumeracao.mapa.MeioDeTransporte;
@@ -35,14 +40,14 @@ public class Trajeto {
 	}
 
 	public void setPontos() {
-//		Client client = ClientBuilder.newClient();
-//		Entity<String> payload = Entity.json("{\"coordinates\":["","",""],\"elevation\":\"true\",\"extra_info\":[\"roadaccessrestrictions\"]}");
-//		Response response = client.target("https://api.openrouteservice.org/v2/directions/"+getTransporteUsado().getDescricao()+"/geojson")
-//		  .request()
-//		  .header("Authorization", "5b3ce3597851110001cf624839b64a140f534a82a4750d447a4df110")
-//		  .header("Accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8")
-//		  .header("Content-Type", "application/json; charset=utf-8")
-//		  .post(payload);
+		Client client = ClientBuilder.newClient();
+		Entity<String> payload = Entity.json("{\"coordinates\":["+getInicio().TransformarVetorEmString()+","+getChegada().TransformarVetorEmString()+"],\"elevation\":\"true\",\"extra_info\":[\"roadaccessrestrictions\"]}");
+		Response response = client.target("https://api.openrouteservice.org/v2/directions/"+getTransporteUsado().getDescricao()+"/geojson")
+		  .request()
+		  .header("Authorization", "5b3ce3597851110001cf624839b64a140f534a82a4750d447a4df110")
+		  .header("Accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8")
+		  .header("Content-Type", "application/json; charset=utf-8")
+		  .post(payload);
 
 		// Tratar GeoJSON
 
@@ -62,18 +67,6 @@ public class Trajeto {
 
 	public void setTransporteUsado(MeioDeTransporte transporteUsado) {
 		this.transporteUsado = transporteUsado;
-	}
-
-	public String[][] gerarLinhaParaDefinirTrajeto() {
-//		ArrayList<ArrayList<String>> linhaDePontos = new ArrayList<ArrayList<String>>();
-//		Scanner sc = new Scanner(System.in);
-//
-//		System.out.print("Inicio: ");
-//		String local = sc.next();
-//		linhaDePontos.add(this.setInicio(local));
-
-		return null;
-
 	}
 
 }
