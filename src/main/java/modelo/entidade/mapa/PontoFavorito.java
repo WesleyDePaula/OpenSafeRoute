4519a55acb57afab5c4ca012eb772190a727e1c1
BrunoRenanPich <87392460+BrunoRenanPich@users.noreply.github.com>
2021-08-17 17:22:06 -0300

    tarefa04
    Alterando PontoFavorito e Ponto

--- src/main/java/modelo/entidade/mapa/PontoFavorito.java
+++ src/main/java/modelo/entidade/mapa/PontoFavorito.java
@@ -1,30 +1,56 @@
 package modelo.entidade.mapa;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import modelo.entidade.usuario.UsuarioCadastrado;
 import modelo.excecao.mapa.StatusInvalidoException;
 
-public class PontoFavorito extends Ponto {
+@Entity
+@Table(name = "ponto_favorito")
 
-	private int idPontoFav;
+public class PontoFavorito extends Ponto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_ponto_favorito")
+	private Long idPontoFav;
+
+	@Column(name = "nome_ponto_favorito", length = 20, nullable = false, unique = false)
 	private String nomePonto;
 
-	public PontoFavorito(int idPontoFav, Ponto ponto, String nomePonto) throws StatusInvalidoException {
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_ponto")
+	private Ponto ponto;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_usuario")
+	private UsuarioCadastrado usuario;
+
+	public PontoFavorito() {}
+
+	public PontoFavorito(Long idPontoFav, Ponto ponto, String nomePonto) throws StatusInvalidoException {
 		super(ponto.getLatitude(), ponto.getLongitude());
-		
+
 		setIdPontoFav(idPontoFav);
 		setNomePonto(nomePonto);
 	}
-	
+
 	public PontoFavorito(Ponto ponto, String nomePonto) throws StatusInvalidoException {
 		super(ponto.getLatitude(), ponto.getLongitude());
 		setNomePonto(nomePonto);
 	}
 
-	
-	public int getIdPontoFav() {
+	public Long getIdPontoFav() {
 		return idPontoFav;
 	}
-	
-	public void setIdPontoFav(int idPontoFav) {
+
+	public void setIdPontoFav(Long idPontoFav) {
 		this.idPontoFav = idPontoFav;
 	}
 
@@ -34,6 +60,23 @@ public class PontoFavorito extends Ponto {
 
 	public void setNomePonto(String nomePonto) {
 		this.nomePonto = nomePonto;
+
+	}
+
+	public Ponto getPonto() {
+		return ponto;
+	}
+
+	public void setPonto(Ponto ponto) {
+		this.ponto = ponto;
+	}
+
+	public UsuarioCadastrado getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(UsuarioCadastrado usuario) {
+		this.usuario = usuario;
 	}
 
 	public static PontoFavorito favoritarPontoENomear(Ponto ponto, String nomePonto) throws StatusInvalidoException {
@@ -41,6 +84,5 @@ public class PontoFavorito extends Ponto {
 		return new PontoFavorito(ponto, nomePonto);
 
 	}
-	
-	
+
 }
