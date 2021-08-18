40418798e306b8fc8ffc9559038124e443f11d03
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-08-17 21:34:20 -0300

    Update Trajeto.java
    Criando método para evitar um Ponto na classe Trajeto

--- src/main/java/modelo/entidade/mapa/Trajeto.java
+++ src/main/java/modelo/entidade/mapa/Trajeto.java
@@ -1,5 +1,7 @@
 package modelo.entidade.mapa;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -67,6 +69,41 @@ public class Trajeto {
 
 	public void setTransporteUsado(MeioDeTransporte transporteUsado) {
 		this.transporteUsado = transporteUsado;
+	}
+
+	private ArrayList<PontoAvaliado> evitarPontos (ArrayList<PontoAvaliado> pontos){
+		ArrayList<PontoAvaliado> evitar = new ArrayList<>();
+		
+		for (int nota = 10; nota >= 0; nota--){
+
+			for (int i = 1; i < pontos.size(); i++){
+				if (pontos.get(i).getMediaDeAvaliacao() < nota){
+					evitar.add(pontos.get(i));
+				}
+			}
+
+			try {
+				
+				Client client = ClientBuilder.newClient();
+				Entity<String> payload = Entity.json("{\"coordinates\":["+getInicio().TransformarVetorEmString()+","+getChegada().TransformarVetorEmString()+"],\"elevation\":\"true\",\"extra_info\":[\"roadaccessrestrictions\"]}");
+				Response response = client.target("https://api.openrouteservice.org/v2/directions/"+getTransporteUsado().getDescricao()+"/geojson")
+				.request()
+				.header("Authorization", "5b3ce3597851110001cf624839b64a140f534a82a4750d447a4df110")
+				.header("Accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8")
+				.header("Content-Type", "application/json; charset=utf-8")
+				.post(payload);
+
+				//evitar Pontos na varavel evitar 
+
+				if (response.getStatus() != 200) {
+					throw new StatusInvalidoException("Ocoreu um erro no requrimento da API");
+				}
+
+			} catch (StatusInvalidoException e) {
+				evitar.removeAll(evitar);
+			}
+		}
+		return evitar;
 	}
 
 }
