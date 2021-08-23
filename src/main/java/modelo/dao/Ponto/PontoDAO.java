88548a286db3def2282268dc4d796dc265a4f166
WesleyDePaula <87441035+WesleyDePaula@users.noreply.github.com>
2021-08-23 17:18:31 -0300

    Adicionando o metodo inserir ponto no PontoDAO

---
+++ src/main/java/modelo/dao/Ponto/PontoDAO.java
@@ -0,0 +1,9 @@
+package modelo.dao.Ponto;
+
+import modelo.entidade.mapa.Ponto;
+
+public interface PontoDAO {
+
+	void adicionarPonto(Ponto ponto);
+	
+}
---
+++ src/main/java/modelo/dao/Ponto/PontoDAOImpl.java
@@ -0,0 +1,63 @@
+package modelo.dao.Ponto;
+
+import java.sql.SQLException;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import modelo.entidade.mapa.Ponto;
+
+public class PontoDAOImpl {
+
+	private SessionFactory conectarBanco() throws SQLException {
+
+		Configuration configuracao = new Configuration();
+
+		configuracao.addAnnotatedClass(modelo.entidade.formulario.Formulario.class);
+		configuracao.addAnnotatedClass(modelo.entidade.mapa.PontoAvaliado.class);
+		configuracao.addAnnotatedClass(modelo.entidade.mapa.PontoFavorito.class);
+		configuracao.addAnnotatedClass(modelo.entidade.mapa.Trajeto.class);
+
+		configuracao.configure("hibernate.cfg.xml");
+
+		ServiceRegistry servico = new StandardServiceRegistryBuilder().applySettings(configuracao.getProperties())
+				.build();
+
+		SessionFactory fabricaSessao = configuracao.buildSessionFactory(servico);
+
+		return fabricaSessao;
+	}
+
+	public void adicionarPonto(Ponto ponto) {
+
+		Session sessao = null;
+
+		try {
+
+			sessao = conectarBanco().openSession();
+			sessao.beginTransaction();
+
+			sessao.save(ponto);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception erro) {
+			erro.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+
+	}
+
+}
