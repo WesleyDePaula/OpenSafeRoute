90b5b48636633d32a6b6116c8c8b99c65512ea3b
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-08-24 10:01:51 -0300

    Removendo extra info para testes

--- src/main/java/modelo/entidade/mapa/Trajeto.java
+++ src/main/java/modelo/entidade/mapa/Trajeto.java
@@ -105,7 +105,7 @@ public class Trajeto {
 		Client client = ClientBuilder.newClient();
 		Entity<String> payload = Entity.json("{\"coordinates\":[" + getInicio().TransformarVetorEmString() + ","
 				+ getChegada().TransformarVetorEmString()
-				+ "],\"elevation\":\"true\",\"extra_info\":[\"steepness\",\"waycategory\",\"waytype\",\"tollways\"]");
+				+ "],\"elevation\":\"true");
 		Response response = client
 				.target("https://api.openrouteservice.org/v2/directions/" + getTransporteUsado().getDescricao()
 						+ "/geojson")
