ebb02ed03493a10bc7084107e52251608ad0b2a2
WesleyDePaula <87441035+WesleyDePaula@users.noreply.github.com>
2021-08-24 14:49:26 -0300

    Criado metodo para inserir um Ponto avaliado

---
+++ src/main/java/modelo/dao/PontoAvaliado/PontoAvaliadoDAO.java
@@ -0,0 +1,11 @@
+package modelo.dao.PontoAvaliado;
+
+import modelo.entidade.mapa.PontoAvaliado;
+
+public interface PontoAvaliadoDAO {
+	
+	void adicionarPontAvaliado(PontoAvaliado pontoAvaliado);
+	
+	void deletarPontAvaliado(PontoAvaliado pontoAvaliado);
+
+}
---
+++ src/main/java/modelo/dao/PontoAvaliado/PontoAvaliadoDAOImpl.java
@@ -0,0 +1,75 @@
+package modelo.dao.PontoAvaliado;
+
+import org.hibernate.Session;
+
+import modelo.entidade.mapa.PontoAvaliado;
+import modelo.factory.conexao.ConexaoFactory;
+
+public class PontoAvaliadoDAOImpl implements PontoAvaliadoDAO {
+
+	private ConexaoFactory fabrica;
+
+	public PontoAvaliadoDAOImpl() {
+		fabrica = new ConexaoFactory();
+	}
+
+	@Override
+	public void adicionarPontAvaliado(PontoAvaliado pontoAvaliado) {
+		Session sessao = null;
+
+		try {
+
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+
+			sessao.save(pontoAvaliado);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception erro) {
+			erro.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+
+	}
+
+	@Override
+	public void deletarPontAvaliado(PontoAvaliado pontoAvaliado) {
+
+		Session sessao = null;
+
+		try {
+
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+
+			sessao.delete(pontoAvaliado);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception erro) {
+			erro.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+
+	}
+
+}
