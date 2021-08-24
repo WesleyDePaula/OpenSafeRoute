bc42ddf50186190cbbfec79ea46d8b744123a359
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-08-24 16:33:24 -0300

    alterando classe Ponto para remover as consultas

--- src/main/java/modelo/entidade/mapa/Ponto.java
+++ src/main/java/modelo/entidade/mapa/Ponto.java
@@ -2,20 +2,12 @@ package modelo.entidade.mapa;
 
 import java.util.ArrayList;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.geojson.FeatureCollection;
-import org.geojson.GeoJsonObject;
 import org.geojson.Point;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
+import modelo.consultaAPI.ConsultaPonto;
 import modelo.excecao.mapa.StatusInvalidoException;
 
 public class Ponto {
@@ -23,45 +15,19 @@ public class Ponto {
 	private int idPonto;
 	private Point LongLatAlt;
 
-	public Ponto(double latitude, double longitude) throws StatusInvalidoException, JsonMappingException, JsonProcessingException {
+	public Ponto(double latitude, double longitude,double altitude)
+			throws StatusInvalidoException, JsonMappingException, JsonProcessingException {
 		this.setLatitude(latitude);
 		this.setLongitude(longitude);
-		this.setAltitude();
+		this.setAltitude(altitude);
 	}
 
 	public Ponto() {
 	}
 
 	public static Ponto informatLocal(String local)
-			throws StatusInvalidoException, JsonMappingException, JsonProcessingException {
-		String localParaURL = local.replaceAll(" ", "%20");
-		Client client = ClientBuilder.newClient();
-		Response response = client.target(
-				"https://api.openrouteservice.org/geocode/search?api_key=5b3ce3597851110001cf624839b64a140f534a82a4750d447a4df110&text="
-						+ localParaURL)
-				.request(MediaType.TEXT_PLAIN_TYPE)
-				.header("Accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8")
-				.get();
-
-		if (response.getStatus() == 4001) {
-			throw new StatusInvalidoException("Erro de valor no paremetro");
-		} else if (response.getStatus() == 4002) {
-			throw new StatusInvalidoException("CabeÃ§alhos de HTTP errados");
-		} else if (response.getStatus() == 4003) {
-			throw new StatusInvalidoException("Problemas ao prover a geometria");
-		} else if (response.getStatus() == 4004) {
-			throw new StatusInvalidoException("Excedeu o nÃºmero de vÃ©rtices permitidos");
-		} else if (response.getStatus() != 200) {
-			throw new StatusInvalidoException("Ocoreu um erro no requrimento da API");
-		}
-
-		GeoJsonObject object = new ObjectMapper().readValue(response.readEntity(String.class), GeoJsonObject.class);
-
-		Ponto ponto = new Ponto();
-
-		ponto.setLongLatAlt((Point) ((FeatureCollection) object).getFeatures().get(0).getGeometry());
-		
-		return ponto;
+			throws JsonMappingException, JsonProcessingException, StatusInvalidoException {
+		return ConsultaPonto.informatLocal(local);
 	}
 
 	public Point getLongLatAlt() {
@@ -76,37 +42,9 @@ public class Ponto {
 		return (int) getLongLatAlt().getCoordinates().getAltitude();
 	}
 
-	public void setAltitude() throws StatusInvalidoException, JsonMappingException, JsonProcessingException {
+	public void setAltitude(double altitude) {
 
-		Client client = ClientBuilder.newClient();
-		Entity<String> payload = Entity
-				.json("{\"format_in\":\"point\",\"format_out\":\"geojson\",\"dataset\":\"srtm\"\"geometry\":" + this.TransformarVetorEmString() + "}");
-		Response response = client.target("https://api.openrouteservice.org/elevation/point").request()
-				.header("Authorization", "5b3ce3597851110001cf624839b64a140f534a82a4750d447a4df110")
-				.header("Accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8")
-				.header("Content-Type", "application/json; charset=utf-8").post(payload);
-
-		if (response.getStatus() == 4001) {
-			throw new StatusInvalidoException("Erro de valor no paremetro");
-		} else if (response.getStatus() == 4002) {
-			throw new StatusInvalidoException("CabeÃ§alhos de HTTP errados");
-		} else if (response.getStatus() == 4003) {
-			throw new StatusInvalidoException("Problemas ao prover a geometria");
-		} else if (response.getStatus() == 4004) {
-			throw new StatusInvalidoException("Excedeu o nÃºmero de vÃ©rtices permitidos");
-		} else if (response.getStatus() != 200) {
-			throw new StatusInvalidoException("Ocoreu um erro no requrimento da API");
-		}
-
-		GeoJsonObject object = new ObjectMapper().readValue(response.readEntity(String.class), GeoJsonObject.class);
-
-		((FeatureCollection) object).getFeatures().get(0).getGeometry();
-
-		Ponto ponto = new Ponto();
-
-		ponto.setLongLatAlt((Point) ((FeatureCollection) object).getFeatures().get(0).getGeometry());
-
-		this.getLongLatAlt().getCoordinates().setAltitude(ponto.getAltitude());
+		this.LongLatAlt.getCoordinates().setAltitude(altitude);
 
 	}
 
